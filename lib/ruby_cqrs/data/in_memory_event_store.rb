8c225b96b0bb1e636f21fc1d4ecc46c571e53f0c
Raven Chen <ravenchen.cn@gmail.com>
2015-04-02 21:28:57 +0800

    renamed methods

--- lib/ruby_cqrs/data/in_memory_event_store.rb
+++ lib/ruby_cqrs/data/in_memory_event_store.rb
@@ -15,26 +15,22 @@ module RubyCqrs
         changes.each do |change|
           key = change[:aggregate_id].to_sym
           if @aggregate_store.has_key? key
-            add_to_old_aggregate key, change
+            update_aggregate key, change
           else
-            create_new_aggregate key, change
+            create_aggregate key, change
           end
         end
       end
 
     private
-      def load_aggregate guid
-
-      end
-
-      def create_new_aggregate key, change
+      def create_aggregate key, change
         @aggregate_store[key] = {
           :type => change[:aggregate_type],
           :version => change[:expecting_version] }
         @event_store[key] = { :events => change[:events] }
       end
 
-      def add_to_old_aggregate key, change
+      def update_aggregate key, change
         @aggregate_store[key][:version] = change[:expecting_version]
         change[:events].each do |event|
           @event_store[key][:events] << event
