867fea8fee27b821a6628379f8a46b986b3b631c
Raven Chen <ravenchen.cn@gmail.com>
2015-04-10 05:46:17 +0800

    added code climate test report

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ group :test do
   gem "rake",         "10.4.2"
   gem "rspec",        "3.2.0"
 end
+
+gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'bundler'
 Bundler.setup(:default, :test)
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
